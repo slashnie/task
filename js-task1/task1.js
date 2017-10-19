46348a4cd30efd700e01ed8f76605c1a0fd40be8
slashnie <1148248468@qq.com>
2017-10-19 15:09:07 +0800

    js任务一

---
+++ js-task1/task1.js
@@ -0,0 +1,46 @@
+var square = document.getElementsByClassName("square");
+var color = [];
+
+function getColor() {
+    for (var i = 0; i < 10; i++) {
+        color[i] = Math.floor(Math.random() * 256);
+    }
+}
+
+color.length = 10;
+var div = [];
+for (var u = 0; u < 9; u++) {
+    div[u] = square[u];
+}
+
+function random() {
+    div.sort(function () {
+        return 0.5 - Math.random()
+    });
+}
+
+function setColor() {
+    div[3].style.backgroundColor = "rgb(" + color[1] + ',' + color[2] + ',' + color[3] + ")";
+    div[4].style.backgroundColor = "rgb(" + color[4] + ',' + color[5] + ',' + color[6] + ")";
+    div[8].style.backgroundColor = "rgb(" + color[7] + ',' + color[8] + ',' + color[9] + ")";
+}
+
+function clear() {
+    div[3].style.backgroundColor = null;
+    div[4].style.backgroundColor = null;
+    div[8].style.backgroundColor = null;
+}
+
+function begin() {
+    this.begin = false;
+    var timer = setInterval(function () {
+            clear();
+            random();
+            getColor();
+            setColor();
+        }
+        , 1000);
+}
+
+
+
